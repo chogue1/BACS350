2450e337692f0cf5dd573aa9cb277d9dc40aab01
chogue1 <70293241+chogue1@users.noreply.github.com>
2020-09-04 02:16:29 -0600

    debugging server
    server couldn't work, had to return

--- bacs350/bacs350/urls.py
+++ bacs350/bacs350/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import HomeView, IndexView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
