d0bfba2d9cb426f50b86cc79f9c62a7f461cde6b
chogue1 <70293241+chogue1@users.noreply.github.com>
2020-09-05 14:48:11 -0600

    final touches

--- superhero/hero/views.py
+++ superhero/hero/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import TemplateView
 
 class HulkView(TemplateView):
-    template_name = 'hulk.html'
+    template_name = 'hero.html'
     def get_context_data(self, **kwargs):
         return { 'hero': 'hulk' }
     
 class WidowView(TemplateView):
-    template_name = 'black_widow.html'
+    template_name = 'hero.html'
     def get_context_data(self, **kwargs):
         return { 'hero': 'black_widow' }
 
