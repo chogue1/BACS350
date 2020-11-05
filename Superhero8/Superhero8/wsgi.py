1e6a7bdadb63e252c2787a1d6af239797f114696
Christian Hogue <70293241+chogue1@users.noreply.github.com>
2020-11-05 09:05:50 -0700

    Copied project 7

--- Superhero8/Superhero8/wsgi.py
+++ Superhero8/Superhero8/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Superhero8 project.
+WSGI config for Superhero7 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Superhero8.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Superhero7.settings')
 
 application = get_wsgi_application()
---
+++ Superhero8/hero/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from hero.views import HeroAddView, HeroListView, HeroEditView, HeroDetailView
+
+urlpatterns = [
+    path('', HeroListView.as_view(), name='hero_list'),
+    path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
+    path('add', HeroAddView.as_view(), name='hero_add'),
+    path('<int:pk>/', HeroEditView.as_view(), name='hero_edit'),
+]
