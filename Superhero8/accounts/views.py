b58e539dcd61fc7a3c8a7301f09fa4e56c031a9b
chogue1 <hogu4987@bears.unco.edu>
2020-11-20 13:45:46 -0700

    added navbar AGAIN

--- Superhero8/accounts/views.py
+++ Superhero8/accounts/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
-from django.forms import ModelForm
-from accounts.models import User
+# accounts/views.py
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
-class RegisterForm(ModelForm):
-    username = forms.CharField(max_length=25)
-    password = forms.CharField(widget=PasswordInput)
-    email = forms.CharField(widget=EmailInput)
-    first_name = forms.CharField(max_length=25)
-    last_name = forms.CharField(max_length=35)
-
-    class Meta:
-        model = User
-        fields = ["username","password","email","first_name","last_name"]
-    
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
