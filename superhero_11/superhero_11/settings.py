f18eb1d0d80e4b83a1c896fea5e439eb79dc8cd7
chogue1 <hogu4987@bears.unco.edu>
2020-11-20 15:27:55 -0700

    clean up

--- superhero_11/superhero_11/settings.py
+++ superhero_11/superhero_11/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '49ulry&pcod0=wm12$t3*eqi_w1$3_e+ou88f4#vvvuo+ppcu8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
